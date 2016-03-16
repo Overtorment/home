19be8cd17d7d51ea4ac94dc86272bccf9bbe0f65
Overtorment <overtorment@gmail.com>
2016-03-16 23:39:50 +0100

    r

--- sc.js
+++ sc.js
@@ -1,43 +1,81 @@
-var countdown = 500;
-setInterval(function(){
-	if ( countdown <= 0 ) return;
+// LOAD followers on page
+
+var scrollto = 0;
+var loadmore = function(){
+    scrollto += 500;
+    window.scrollTo(0, scrollto);
+    setTimeout(function(){
+		if ($$('.sc-button-follow').length < 4500) {			
+			console.log('Loaded ', $$('.sc-button-follow').length);
+			return loadmore();
+		}
+    }, 500);
+}
+
+
+
+
+
+
+// UNFOLLOW followed people
+
+var countdown_unfollow = 150;
+var dounfollow = function(){
+	if ( countdown_unfollow <= 0 ) return;
 	var a = $('.sc-button-follow') ;
-	if (a.innerText == "Follow") {
-		a.click();
-		countdown--;
-		console.log("Left: " + countdown);
-	}  else {
-		console.log("Removing... Left: " + countdown);
+	if (a && a.innerText == "Following") {
+		var nickname = false;
+		if (a.parentNode.parentNode.childNodes[2]) {
+			nickname = a.parentNode.parentNode.childNodes[2].innerText;
+			a.click();
+			countdown_unfollow--;
+			localStorage[nickname] = 'unfollowed';
+			console.log('Unfollowing ', nickname);
+		}
 		a.remove();
-	}
-}, 5000)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-var countdown = 200;
-
-var doit = function(){
-	if ( countdown <= 0 ) return;
-	var a = $('.sc-button-follow') ;
-	if (a.innerText == "Follow") {
-		a.click();
-		countdown--;
-		console.log("Left: " + countdown);
-		setTimeout(function(){doit()}, 6000)
-	}  else {
-		console.log("Removing... Left: " + countdown);
+		console.log("Left to unfollow: " + countdown_unfollow);
+		setTimeout(function(){dounfollow()}, 1000)
+	}  else if (a) {
+		console.log("Removing... Left to unfollow: " + countdown_unfollow);
 		a.remove();
-		doit();
+		dounfollow();
 	}
 }
+
+
+
+
+// FOLLOW people
+
+
+var countdown = 100;
+
+var dofollow = function(){
+	if ( countdown <= 0 ) return;
+	var a = $('.sc-button-follow') ;
+	if (a.innerText == "Follow") {
+		var nickname = false;
+		nickname = a.parentNode.parentNode.childNodes[2].innerText;
+		if (localStorage[nickname] && localStorage[nickname] == 'unfollowed'){
+			// should skip, we followed him before
+			a.remove();
+			return dofollow();
+		} else {
+			countdown--;
+			console.log('followin ', nickname);
+			
+			setTimeout(function(){ a.click(); }, 0);
+			setTimeout(function(){ a.remove(); },1000);
+		}
+
+		console.log("Left: " + countdown);
+		setTimeout(function(){dofollow()}, 6000)
+	}  else {
+		console.log("Removing... Left: " + countdown);
+		a.remove();
+		dofollow();
+	}
+}
+
+
+
