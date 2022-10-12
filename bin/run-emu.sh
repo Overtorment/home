0338a3bd0d4542835f3d70c4443832206589f18f
overtorment <overtorment@gmail.com>
2022-10-12 18:11:40 +0100

    REF

--- bin/run-emu.sh
+++ bin/run-emu.sh
@@ -1,7 +1,7 @@
 #export ANDROID_HOME=$HOME/Android/Sdk
 #export ANDROID_SDK_ROOT="/home/overtorment/Android/Sdk/"
 
-cd /home/overtorment/Android/Sdk/emulator
+cd ~/Android/Sdk/emulator
 ./emulator -list-avds
 #./emulator -avd fukken_PIE -logcat-output /dev/stdout  -camera-front webcam0  -camera-back  webcam0 | grep -v GnssLocationProvider | ack --passthru "ReactNative|AndroidRuntime"
 #./emulator -avd Pixel_5_API_30  -logcat-output /dev/stdout  -camera-front webcam0  -camera-back  webcam0 -partition-size 2047  -gpu swiftshader_indirect  -noaudio -no-boot-anim -no-snapshot-load  | egrep -v 'GnssLocationProvider|GnssHAL_GnssInterface' | tee emulator.log |  ack --passthru "ReactNative|AndroidRuntime|GoLog"
