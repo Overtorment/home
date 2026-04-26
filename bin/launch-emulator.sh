1eb8bfbd9186fcf211ee99ad959a207e57ee65f1
Overtorment <overtorment@gmail.com>
2026-04-26 16:46:52 +0100

    ref

---
+++ bin/launch-emulator.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+~/Android/Sdk/emulator/emulator -avd  Medium_Phone_API_36.1 -camera-front webcam0 -camera-back webcam0
+#~/Android/Sdk/emulator/emulator -avd  Pixel_API_29_AOSP  -camera-front webcam0 -camera-back webcam0
